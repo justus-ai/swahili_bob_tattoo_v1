f6ab0836184c3ca09786173d0d347704cba25992
Justus marwa <83753891+justus-ai@users.noreply.github.com>
2026-02-04 09:17:10 +0000

    edit to remove remaining v1 reference

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/swahili_bob_tattoo_v1/.venv"
+set -gx VIRTUAL_ENV "/workspaces/swahili_bob_tattoo/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- swahili_bob_tattoo/settings.py
+++ swahili_bob_tattoo/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'justus-ai-swahili-bob-tattoo-v1.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'justus-ai-swahili-bob-tattoo.onrender.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
