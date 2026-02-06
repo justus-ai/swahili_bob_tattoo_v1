d0a844ebd035e9b59ea069950386caffe6f8e823
Justus marwa <83753891+justus-ai@users.noreply.github.com>
2026-02-06 12:26:44 +0000

    Explicitly install psycopg2-binary in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Install system dependencies for psycopg2
+apt-get update
+apt-get install -y libpq-dev
+
+# Install Python packages
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Django setup
 python manage.py collectstatic --no-input
 python manage.py migrate
